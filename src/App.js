0e0bd0a255c3fdd5654e5ca3fd406336cdcf748c
shanemmay <shanemarvinmay@hotmail.com>
2018-08-19 15:03:20 -0500

    init commit with 'coming soon message'

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Shane Marvin May's Site Coming Soon!</h1>
+        </header>
+        <p className="App-intro">
+          email me at shanemaygunlogson@gmail.com if you see this.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
